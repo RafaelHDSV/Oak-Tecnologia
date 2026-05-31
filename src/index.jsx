8e033931157ef4b592b9b697e559250222eec9fa
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-31 03:35:02 -0300

    feat: add Vieira Analytics integration

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { VieiraAnalytics } from '@vieira/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<App />
+	  <VieiraAnalytics projectKey='oak-tecnologia' />
 	</React.StrictMode>
 );
